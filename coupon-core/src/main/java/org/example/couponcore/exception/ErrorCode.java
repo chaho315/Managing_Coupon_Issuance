eb8d6c1eb8cb54ba50d668475afee0183ccec307
chaho510 <chaho510@naver.com>
2025-05-01 23:47:45 +0900

    쿠폰발급 코어로직 및 exception 처리 testcase 생성

---
+++ coupon-core/src/main/java/org/example/couponcore/exception/ErrorCode.java
@@ -0,0 +1,11 @@
+package org.example.couponcore.exception;
+
+public enum ErrorCode {
+    INVAILD_COUPON_ISSUE_QUANTITY,
+
+    INVAILD_COUPON_ISSUE_DATE,
+
+    COUPON_NOT_EXIST,
+
+    DUPLICATED_COUPON_ISSUE;
+}
--- coupon-core/src/main/java/org/example/couponcore/model/Coupon.java
+++ coupon-core/src/main/java/org/example/couponcore/model/Coupon.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.couponcore.exception.CouponIssueException;
+import org.example.couponcore.exception.ErrorCode;
 
 import java.time.LocalDateTime;
 
@@ -43,4 +45,27 @@ public class Coupon extends BaseTimeEntity{
 
     @Column(nullable = false)
     private LocalDateTime dateIssueEnd;
+
+    public boolean availableIssueQuantity(){
+        if (totalQuantity == null) {
+            return true;
+        }
+        return totalQuantity > issuedQuantity;
+    }
+
+    public boolean availableIssueDate(){
+        LocalDateTime now = LocalDateTime.now();
+        return dateIssueStart.isBefore(now) && dateIssueEnd.isAfter(now);
+    }
+
+    public void issue(){
+        if(!availableIssueQuantity()){
+            throw new CouponIssueException(ErrorCode.INVAILD_COUPON_ISSUE_QUANTITY, "발급 가능한 수량을 초과합니다. total : %s, issued: %s".formatted(totalQuantity, issuedQuantity));
+        }
+        if(!availableIssueDate()){
+            throw new CouponIssueException(ErrorCode.INVAILD_COUPON_ISSUE_DATE,"발급 가능한 일자가 아닙니다. request : %s, issueStart : %s, issueEnd : %s".formatted(LocalDateTime.now(), dateIssueStart, dateIssueEnd));
+        }
+        issuedQuantity++;
+    }
+
 }
